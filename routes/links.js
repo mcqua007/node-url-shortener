b142030445147700f0fd364a3a1e7022e4b7535c
mcqua007 <sean@decrypteddesign.com>
2021-04-21 13:54:12 -0700

    fix | added check for empty

--- routes/links.js
+++ routes/links.js
@@ -25,39 +25,48 @@ router.post('/', function(req, res, next) {
   var { original } = req.body;
   var shortCode = nanoid();
   var shortUrl = `http://localhost:3200/` + shortCode;
-  Link.findOne({ original: original })
-    .then(data => {
-      if (!data) {
-        Link.create({ original: original, shortUrl: shortUrl, shortCode: shortCode })
-          .then(data => {
-            res.status(200).json({
-              success: true,
-              message: 'OK',
-              data: data
-            });
-          })
-          .catch(e => {
-            res.status(500).json({
-              success: false,
-              message: 'error',
-              error: e
-            });
-          });
-      } else {
-        res.status(200).json({
-          success: true,
-          message: 'OK',
-          data: data
-        });
-      }
-    })
-    .catch(e => {
-      res.status(500).json({
-        success: false,
-        message: 'error',
-        error: e
-      });
+  if (!original) {
+    res.status(400).json({
+      success: false,
+      message: 'error',
+      error: 'original cannot be empty'
     });
+    next();
+  } else {
+    Link.findOne({ original: original })
+      .then(data => {
+        if (!data) {
+          Link.create({ original: original, shortUrl: shortUrl, shortCode: shortCode })
+            .then(data => {
+              res.status(200).json({
+                success: true,
+                message: 'OK',
+                data: data
+              });
+            })
+            .catch(e => {
+              res.status(500).json({
+                success: false,
+                message: 'error',
+                error: e
+              });
+            });
+        } else {
+          res.status(200).json({
+            success: true,
+            message: 'OK',
+            data: data
+          });
+        }
+      })
+      .catch(e => {
+        res.status(500).json({
+          success: false,
+          message: 'error',
+          error: e
+        });
+      });
+  }
 });
 
 module.exports = router;
