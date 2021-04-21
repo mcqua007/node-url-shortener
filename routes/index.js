34b7cf269118ca9d0ca74686d7bde670ed6cc2a8
mcqua007 <sean@decrypteddesign.com>
2021-04-21 12:03:29 -0700

    feat | added get route to redirect to redirect Url

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const Link = require('../models/Link');
+var requestUrl = require('../helpers/requestUrl');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/:hash', function(req, res, next) {
+  var { hash } = req.params;
+  if (hash !== 'links') {
+    Link.findOne({ original: requestUrl(req) }).then(data => {
+      res.redirect(301, data.redirectUrl);
+      next();
+    });
+  } else {
+    next();
+  }
 });
 
 module.exports = router;
