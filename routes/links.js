9355fce2c89f7fae4b94cdf6c740df931a23bf0d
mcqua007 <sean@decrypteddesign.com>
2021-04-21 13:47:12 -0700

    feat | using nanoid

--- routes/links.js
+++ routes/links.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const Link = require('../models/Link');
+const { customRandom, urlAlphabet, customAlphabet, nanoid } = require('nanoid');
 
 router.get('/', function(req, res, next) {
   Link.find().then(data => {
@@ -14,14 +15,27 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
   var { original } = req.body;
-  var { redirectUrl } = req.body;
-
-  Link.create({ original: original, redirectUrl: redirectUrl }).then(data => {
-    res.status(200).json({
-      success: true,
-      message: 'OK',
-      data: data
-    });
+  //var { redirectUrl } = req.body;
+  var shortCode = nanoid();
+  var shortUrl = `http://localhost:3200/` + shortCode;
+  Link.findOne({ original: original }).then(data => {
+    console.log('findone ', data);
+    if (!data) {
+      Link.create({ original: original, shortUrl: shortUrl, shortCode: shortCode }).then(data => {
+        res.status(200).json({
+          success: true,
+          message: 'OK',
+          data: data
+        });
+      });
+    } else {
+      console.log('exists');
+      res.status(200).json({
+        success: true,
+        message: 'OK',
+        data: data
+      });
+    }
   });
 });
 
