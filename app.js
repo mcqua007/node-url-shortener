05874e9254a89409563f8fb6622c8c27530655f2
mcqua007 <sean@decrypteddesign.com>
2021-04-21 11:39:17 -0700

    feat | connect to in memory database via mongoose

--- app.js
+++ app.js
@@ -2,9 +2,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 
+async function initDB() {
+  const mongod = new MongoMemoryServer();
+
+  const uri = await mongod.getUri();
+  const port = await mongod.getPort();
+  const dbPath = await mongod.getDbPath();
+  const dbName = await mongod.getDbName();
+
+  mongod.getInstanceInfo(); // return Object with instance data
+  mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+}
+
+initDB();
 var app = express();
 
 app.use(logger('dev'));
