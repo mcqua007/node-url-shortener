5e74ce0f706b787783a8774836a3c08f8de97b40
mcqua007 <sean@decrypteddesign.com>
2021-04-24 18:05:13 -0700

    feat | add count auto-increment to model

--- models/Link.js
+++ models/Link.js
@@ -1,13 +1,15 @@
 var mongoose = require('mongoose');
+var AutoIncrement = require('mongoose-sequence')(mongoose);
+
 const Schema = mongoose.Schema;
 
-const Link = mongoose.model(
-  'Link',
-  new Schema({
-    shortCode: String,
-    shortUrl: String,
-    original: String
-  })
-);
+const LinkSchema = new Schema({
+  shortCode: String,
+  shortUrl: String,
+  original: String
+});
+LinkSchema.plugin(AutoIncrement, { inc_field: 'count', start_seq: 1 });
+
+const Link = mongoose.model('Link', LinkSchema);
 
 module.exports = Link;
